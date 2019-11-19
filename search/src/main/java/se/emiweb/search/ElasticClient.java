b3ce80e755994123e9fdb0037f5593238a12b771
Illya Kyrychenko <illya.k@hotmail.com>
2019-11-19 15:23:07 +0100

    elastic API

--- search/src/main/java/se/emiweb/search/ElasticClient.java
+++ search/src/main/java/se/emiweb/search/ElasticClient.java
@@ -2,22 +2,32 @@ package se.emiweb.search;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Date;
 
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
-public class ElasticClient {
+public class ElasticClient 
+{
 	void Client()
 	{
 		System.out.println("TEST1");		
 		
 		TransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
 		// on startup
-		try {
+		try 
+		{
 			client.addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
 		}
 		catch(IOException e){
@@ -25,21 +35,61 @@ public class ElasticClient {
 			e.getLocalizedMessage();
 		}
 		
+		
+		//Insert JSON to elasticsearch
 		String json = "{" +
 		        "\"user\":\"kimchy\"," +
 		        "\"postDate\":\"2013-01-30\"," +
 		        "\"message\":\"trying out Elasticsearch\"" +
 		    "}";
 		
+		String json1 = "{" +
+		        "\"user\":\"Test\"," +
+		        "\"postDate\":\"2022-01-30\"," +
+		        "\"message\":\"Hello\"" +
+		    "}";
 		
-		IndexResponse response = client.prepareIndex("twitter", "_doc")
+		
+		IndexResponse info = client.prepareIndex("twitter", "_doc", "1")
 		        .setSource(json, XContentType.JSON)
 		        .get();
 		
+		info = client.prepareIndex("twitter", "_doc", "2")
+		        .setSource(json1, XContentType.JSON)
+		        .get();
+		
 		System.out.println("TEST2");
-		// on shutdown
 
-		//client.close();
+		
+		//Get from elastic search by 1 id at the time
+		/*GetResponse response = client.prepareGet("twitter", "_doc", "1").get();		
+		System.out.println(response);	
+		response = client.prepareGet("twitter", "_doc", "2").get();	
+		System.out.println(response);*/
+		
+		//Get from elastic search by multiple IDs
+		/*MultiGetResponse multiGetItemResponses = client.prepareMultiGet()
+			    .add("twitter", "_doc", "1", "2")                    
+			    .get();
+		
+		for (MultiGetItemResponse itemResponse : multiGetItemResponses)
+		{ 
+		    GetResponse multiResponse = itemResponse.getResponse();
+		    if (multiResponse.isExists()) {                      
+		        System.out.println(multiResponse.getSourceAsString()); 
+		    }
+		}*/
+		
+		//search query
+		SearchResponse search_response = client.prepareSearch("twitter")
+				.setPostFilter(QueryBuilders.matchQuery("user", "Test"))
+				.get();
+		
+		System.out.println(search_response);
+		
+		// on shutdown
+		client.close();
 		
 	}
 }
+
