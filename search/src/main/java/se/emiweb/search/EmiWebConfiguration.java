5ca3e6dff5e0d29746ad9a604b185e43aab792f3
williamg <ganrotwilliam@gmail.com>
2019-11-21 10:49:02 +0100

    structure and stuff

---
+++ search/src/main/java/se/emiweb/search/EmiWebConfiguration.java
@@ -0,0 +1,51 @@
+package se.emiweb.search;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "se.emiweb.search.repository")
+public class EmiWebConfiguration {
+	
+	@Bean
+	public Client client() throws UnknownHostException {
+		
+		
+		Settings settings = Settings.builder()
+		        .put("cluster.name", "EmiWeb2_Cluster")
+		        .put("client.transport.sniff", true)
+		        .put("node.name", "node-1")
+		        .build();
+		
+		TransportClient client = new PreBuiltTransportClient(settings)
+		        .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
+		
+		
+		
+		String json = "{" +
+		        "\"user\":\"kimchy\"," +
+		        "\"postDate\":\"2013-01-30\"," +
+		        "\"message\":\"trying out Elasticsearch\"" +
+		    "}";
+		
+		IndexResponse info = client.prepareIndex("usmgbg_index", "usmgbg_type", "1")
+		        .setSource(json, XContentType.JSON)
+		        .get();
+		
+
+
+		return client;
+	}
+}
--- search/src/main/java/se/emiweb/search/SearchApplication.java
+++ search/src/main/java/se/emiweb/search/SearchApplication.java
@@ -1,9 +1,11 @@
 package se.emiweb.search;
 
 import org.springframework.boot.SpringApplication;
-import java.lang.System;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"se.emiweb.search"})
@@ -12,7 +14,6 @@ public class SearchApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SearchApplication.class, args);
-		new ElasticClient().Client();
 	}
 
 }
