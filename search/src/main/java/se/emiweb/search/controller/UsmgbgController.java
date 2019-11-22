f5c55d0938405da24259c931a2f4c8a920bd284d
williamg <ganrotwilliam@gmail.com>
2019-11-22 14:25:34 +0100

    Add search-functions to usmgbg_index

--- search/src/main/java/se/emiweb/search/controller/UsmgbgController.java
+++ search/src/main/java/se/emiweb/search/controller/UsmgbgController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 
 import se.emiweb.search.model.Usmgbg;
 import se.emiweb.search.repository.UsmgbgRepository;
@@ -35,26 +37,82 @@ public class UsmgbgController {
 	
 	private  ElasticsearchOperations elasticsearchOperations;
 
-	
-
 	@GetMapping("/all")
 	public SearchResponse findAll() {
+		
+		QueryBuilder query = QueryBuilders.matchAllQuery();
+				
 		SearchResponse search_response = client.prepareSearch("usmgbg_index")
-				.setPostFilter(QueryBuilders.matchAllQuery())
+				.setPostFilter(query)
 				.get();
 		
 		return search_response;
 	} 
 
-	@GetMapping("/byexaktname/{name}")
+	@GetMapping("/byexactname/{name}")
 	public SearchResponse findByExactName(@PathVariable String name) {
+		
+		QueryBuilder query = QueryBuilders.matchQuery("Name", name)
+				.operator(MatchQueryBuilder.DEFAULT_OPERATOR.AND);
 		
 		SearchResponse response = client.prepareSearch("usmgbg_index")
 		        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-		        .setQuery(QueryBuilders.matchQuery("Name", name).operator(MatchQueryBuilder.DEFAULT_OPERATOR.AND))	//term to match
+		        .setQuery(query)	//term to match
 		        .setFrom(0).setSize(100).setExplain(true)			//return max 100 results
 		        .get();	
 		
 		return response;
 	}
+	
+	@GetMapping("/byfuzzyname/{name}")
+	public SearchResponse findByFuzzyName(@PathVariable String name) {
+		
+		
+		QueryBuilder query = QueryBuilders.matchQuery("Name", name)
+				.operator(MatchQueryBuilder.DEFAULT_OPERATOR.AND)
+				.fuzziness("AUTO");
+		
+		SearchResponse response = client.prepareSearch("usmgbg_index")
+		        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+		        .setQuery(query)	//term to match
+		        .setFrom(0).setSize(100).setExplain(true)			//return max 100 results
+		        .get();	
+		
+		return response;
+	}
+	
+	@GetMapping("/byid/{id}")
+	public SearchResponse findById(@PathVariable String id) {
+		
+		QueryBuilder query = QueryBuilders.matchQuery("ID", id );
+		
+		SearchResponse response = client.prepareSearch("usmgbg_index")
+				.setQuery(query)
+				.setFrom(0).setSize(100).setExplain(true)
+				.get();
+		
+		return response;
+	}
+	
+	
+	//Id should notbe fuzzy. Name and proffesion shoould be fuzzy
+	@GetMapping("/allfields/{text}")
+	public SearchResponse findByAllField(@PathVariable String text) {
+
+		MultiMatchQueryBuilder query = QueryBuilders.multiMatchQuery(
+				text
+				, "ID", "Name", "Profession", "Country", "FileName")
+				.operator(MatchQueryBuilder.DEFAULT_OPERATOR.AND);
+
+
+		
+		SearchResponse response = client.prepareSearch("usmgbg_index")
+		        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+		        .setQuery(query)	//term to match
+		        .setFrom(0).setSize(100).setExplain(true)			//return max 100 results
+		        .get();	
+		
+		return response;
+	}
+
 }
